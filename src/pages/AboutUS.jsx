35c5104ca1e2e7eea670f638b418486fd5adcdc0
Nirmal <nirmalsep2001@gmail.com>
2025-04-14 18:20:21 +0530

    Updated with spelling mismatch

--- src/pages/AboutUS.jsx
+++ src/pages/AboutUS.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./AboutUs.css";
+import "./AboutUS.css";
 import aboutGif from "../assets/About.gif"
 const AboutUs = () => {
   return (
